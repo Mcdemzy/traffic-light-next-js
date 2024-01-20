03ea2e4f23897b4fc8ebd5a2ed0346ef94f73d34
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-21 00:16:14 +0100

    Setting timeouts

--- components/TrafficLight.tsx
+++ components/TrafficLight.tsx
@@ -1,17 +1,30 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 type Light = "red" | "yellow " | "green";
 
 function TrafficLight() {
   const [light, setLight] = useState<Light>("red");
 
+  useEffect(() => {
+    const timer = setTimeout(() => {}, 2000);
+  }, [light]);
+
   return (
     <div className="flex flex-col gap-5 border p-10">
-      <div className={`w-40 h-40 bg-red-500 rounded-full`}></div>
-      <div className={`w-40 h-40 bg-yellow-500 rounded-full`}></div>
-      <div className={`w-40 h-40 bg-green-500 rounded-full`}></div>
+      <div
+        className={`w-40 h-40 ${light === "red" && "bg-red-500"} rounded-full`}
+      ></div>
+      <div
+        className={`w-40 h-40 
+        ${light === "yellow" && "bg-yellow-500"} rounded-full`}
+      ></div>
+      <div
+        className={`w-40 h-40 ${
+          light === "green" && "bg-green-500"
+        }  rounded-full`}
+      ></div>
     </div>
   );
 }
