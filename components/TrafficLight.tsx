03d94ff177b88719641e3365622892fad929ba46
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-01-20 20:18:48 +0100

    Defining time outs

--- components/TrafficLight.tsx
+++ components/TrafficLight.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+
+type Light = "red" | "yellow " | "green";
 
 function TrafficLight() {
+  const [light, setLight] = useState<Light>("red");
+
   return (
-    <div className="flex flex-col gap-5">
-      <div className="w-40 h-40 bg-red-500"></div>
-      <div className="w-40 h-40 bg-yellow-500"></div>
-      <div className="w-40 h-40 bg-green-500"></div>
+    <div className="flex flex-col gap-5 border p-10">
+      <div className={`w-40 h-40 bg-red-500 rounded-full`}></div>
+      <div className={`w-40 h-40 bg-yellow-500 rounded-full`}></div>
+      <div className={`w-40 h-40 bg-green-500 rounded-full`}></div>
     </div>
   );
 }
